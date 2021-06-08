bc1e99c9b7950b700bc39ca98bde4fe16807f1e2
William Berry <76190311+wberry86@users.noreply.github.com>
2021-06-07 22:39:25 -0500

    updated readme

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "employee_tracker",
+    });
+
+
+
+    module.exports = db;
--- server.js
+++ server.js
@@ -1,54 +1,164 @@
-const mysql = require('mysql2');
-var inquirer = require('inquirer');
-const cTable = require('console.table');
 const figlet = require("figlet");
 const chalk = require("chalk");
 
+const mysql = require("mysql2");
+var inquirer = require("inquirer");
+
+const db = require("./db/connection");
+const cTable = require("console.table");
+
+
+
+
 // generates terminal graphics
 console.log(
-  chalk.green(
-    figlet.textSync("Employee Manager", { horizontalLayout: "full" })
-  )
+  chalk.blue(figlet.textSync("Employee Manager", { horizontalLayout: "full" }))
 );
 
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test'
-  });
-   
-  // simple query
-  connection.query(
-    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-    }
-  );
-   
-  // with placeholder
-  connection.query(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    ['Page', 45],
-    function(err, results) {
-      console.log(results);
-    }
-  );
+// Start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  choice();
+});
 
+  function choice() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do??",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit"
+        ],
+      },
+    ])
+    .then(function (answers) {
+      if (answers.choice === "View all departments") {
+        viewDepartments();
+      }
+      else if (answers.choice === "View all roles") {
+        viewRoles();
+      }
+      else if (answers.choice === "View all employees") {
+        viewEmployees();
+      }
+      else if (answers.choice === "Add a role") {
+        addRole();
+      }
+      else if (answers.choice === "Add an employee") {
+        addEmployee();
+      }
+      else if (answers.choice === "Update an employee role") {
+        updateRole();
+      }
+      else {
+        exit();
+      }
+    });
+  }
+    
+  
+  
 
+  // view all departments
+  function viewDepartments() {
+    var query = "SELECT * FROM department";
+    db.query(query, function (err, res) {
+      if (err) throw err;
+      console.log(res.length + " departments found!");
+      console.table("All Departments:", res);
+      choice();
+    })
+  }
+  
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
+  // view all employees in the database
+  function viewEmployees() {
+    var query = "SELECT * FROM employee";
+    db.query(query, function (err, res) {
+      if (err) throw err;
+      console.log(res.length + " employees found!");
+      console.table("All Employees:", res);
+      choice();
+    });
+  }
+
+  // view roles
+  function viewRoles() {
+    var query = "SELECT * FROM role";
+    db.query(query, function (err, res) {
+      if (err) throw err;
+      console.log(res.length + " roles found!");
+      console.table("All Roles:", res);
+      choice();
+    });
+  }
+
+  // add a role
+  function addRole() {
+    db.query("SELECT * FROM department", function(err, res) {
+      if (err) throw err;
+
+      inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "newRole",
+          message: "What role would you like to add?"
+        },
+        {
+         type: "input",
+         name: "salary",
+         message: "What is the salary for this role?" 
+        },
+        {
+          name: 'Department',
+          type: 'list',
+          choices: function() {
+              var deptArry = [];
+              for (let i = 0; i < res.length; i++) {
+              deptArry.push(res[i].name);
+              }
+              return deptArry;
+          },
+      }
+  ]).then(function (answers) {
+      let department_id;
+      for (let i = 0; i < res.length; i++) {
+          if (res[i].name == answers.Department) {
+              department_id = res[i].id;
+          }
+      }
+
+      db.query(
+          'INSERT INTO role SET ?',
+          {
+              title: answers.newRole,
+              salary: answers.salary,
+              department_id: department_id
+          },
+          function (err, res) {
+              if(err)throw err;
+              console.log('Your new role has been added!');
+              console.table('All Roles:', res);
+              options();
+          })
   })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+})
+};
+
+  // exit app
+  function exit() {
+    db.end();
+  };
+
+  
